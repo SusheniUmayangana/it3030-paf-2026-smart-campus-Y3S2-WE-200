73b412d62d6ab29b6416b4cabd0591a19e9604c1
dilumhapugoda <dilumhapugoda@gmail.com>
2026-04-19 23:53:51 +0530

    input field css fixed

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,5 @@
+import colors from 'tailwindcss/colors';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -5,7 +7,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        surface: colors.slate,
+        primary: colors.indigo,
+        accent: colors.emerald,
+      }
+    },
   },
   plugins: [],
 }
